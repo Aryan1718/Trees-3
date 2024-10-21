52e3fc3648f8d0e685ee44dce86569bbf00a7b92
aryan <aryanpandit1718@gmail.com>
2024-10-21 02:52:06 -0700

    Trees-3 Done.

--- Sample.java
+++ Sample.java
@@ -5,3 +5,65 @@
 
 
 // Your code here along with comments explaining your approach
+//101. Symmetric Tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None): //DFS T.C->O(N),S.C->O(H)
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    flag = True
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        self.flag = True
+        self.dfs(root.left,root.right)
+        return self.flag
+    def dfs(self,left,right):
+        if left == None and right == None:
+            return
+        if left == None or right == None:
+            self.flag = False
+            return
+        if left.val != right.val:
+            self.flag = False
+            return
+        self.dfs(left.left,right.right)
+        self.dfs(left.right,right.left)
+
+//BFS
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object): //T.C->O(N),S.C->O(N/2)
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        queue = deque()
+        queue.append(root.left)
+        queue.append(root.right)
+
+        while len(queue) != 0:
+            left = queue.popleft()
+            right = queue.popleft()
+
+            if left == None and right == None:
+                continue
+            if left == None or right == None:
+                return False
+            if left.val != right.val:
+                return False
+            queue.append(left.left)
+            queue.append(right.right)
+            queue.append(left.right)
+            queue.append(right.left)
+        return True
+        
